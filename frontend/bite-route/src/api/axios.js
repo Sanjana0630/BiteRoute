7e8bef9ce1f0c407d00c1fdf9e85ad0133fbde1f
Sanjana0630 <sanjanamoralwar17@gmail.com>
2026-02-05 17:00:54 +0530

    Update frontend axios configuration

--- frontend/bite-route/src/api/axios.js
+++ frontend/bite-route/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://biteroute.onrender.com',
+    baseURL: 'http://127.0.0.1:8000',
     headers: {
         'Content-Type': 'application/json',
     },
